1081d06a35876acbb56111262c3251fbf9d36d6e
Peter Shafer <peter.shafer@gmail.com>
2023-09-26 07:54:44 -0700

    Add more cypress tests and update github workflow for automated testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -50,4 +50,85 @@ describe('Browser Page for Tic Tac Toe', () => {
       firstButton.should('not.be.disabled');
     });
   });
+  describe('full gameplay', () => {
+    it('should end the game when x wins', () => {
+      cy.get('#game table button').eq(0).click();
+      cy.get('#game table button').eq(3).click();
+      cy.get('#game table button').eq(1).click();
+      cy.get('#game table button').eq(4).click();
+      cy.get('#game table button').eq(2).click();
+      cy.get('#game table button').each((button) => {
+        cy.wrap(button).should('be.disabled');
+      });
+      cy.get('#game p').should('have.text', 'Game over');
+      cy.get('#game h1').should('have.text', 'x has won the game!');
+    });
+    it('should end the game when o wins', () => {
+      cy.get('#game table button').eq(3).click();
+      cy.get('#game table button').eq(0).click();
+      cy.get('#game table button').eq(4).click();
+      cy.get('#game table button').eq(1).click();
+      cy.get('#game table button').eq(6).click();
+      cy.get('#game table button').eq(2).click();
+
+      cy.get('#game table button').each((button) => {
+        cy.wrap(button).should('be.disabled');
+      });
+      cy.get('#game p').should('have.text', 'Game over');
+      cy.get('#game h1').should('have.text', 'o has won the game!');
+    });
+    it("should end the game if there's a draw", () => {
+      cy.get('#game table button').eq(0).click();
+      cy.get('#game table button')
+        .eq(0)
+        .should('have.text', 'x')
+        .should('be.disabled');
+      cy.get('#game table button').eq(3).click();
+      cy.get('#game table button')
+        .eq(3)
+        .should('have.text', 'o')
+        .should('be.disabled');
+      cy.get('#game table button').eq(1).click();
+      cy.get('#game table button')
+        .eq(1)
+        .should('have.text', 'x')
+        .should('be.disabled');
+      cy.get('#game table button').eq(4).click();
+      cy.get('#game table button')
+        .eq(4)
+        .should('have.text', 'o')
+        .should('be.disabled');
+      cy.get('#game table button').eq(5).click();
+      cy.get('#game table button')
+        .eq(5)
+        .should('have.text', 'x')
+        .should('be.disabled');
+      cy.get('#game table button').eq(2).click();
+      cy.get('#game table button')
+        .eq(2)
+        .should('have.text', 'o')
+        .should('be.disabled');
+      cy.get('#game table button').eq(7).click();
+      cy.get('#game table button')
+        .eq(7)
+        .should('have.text', 'x')
+        .should('be.disabled');
+      cy.get('#game table button').eq(8).click();
+      cy.get('#game table button')
+        .eq(8)
+        .should('have.text', 'o')
+        .should('be.disabled');
+      cy.get('#game table button').eq(6).click();
+      cy.get('#game table button')
+        .eq(6)
+        .should('have.text', 'x')
+        .should('be.disabled');
+
+      cy.get('#game table button').each((button) => {
+        cy.wrap(button).should('be.disabled');
+      });
+      cy.get('#game p').should('have.text', 'Game over');
+      cy.get('#game h1').should('have.text', "It's a draw game!");
+    });
+  });
 });
